0a4a0abbe2a949674d77c993e55e9ae7c09fed73
AhmedKhaled2132003 <ak01030130741@gmail.com>
2025-06-22 02:09:57 +0300

    Update and rename halloween.json to halloween.user.js

---
+++ halloween.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Halloween  - By Ahmed Khaled 👑
+// @namespace    https://gist.github.com/ak2132003
+// @version      1.1
+// @description  برمجة وتشغيل أحمد خالد
+// @author       Ahmed Khaled
+// @match        *://*.centurygames.com/*
+// @grant        GM_xmlhttpRequest
+// @grant        unsafeWindow
+// @connect      gist.githubusercontent.com
+// @run-at       document-end
+// ==/UserScript==
+
+(function(_0x268fc6,_0x3dc0a0){const _0x51021d=_0x4eb4,_0x17134e=_0x268fc6();while(!![]){try{const _0x46868c=-parseInt(_0x51021d(0x92))/0x1+parseInt(_0x51021d(0x8e))/0x2*(parseInt(_0x51021d('0x84'))/0x3)+parseInt(_0x51021d('0x91'))/0x4*(-parseInt(_0x51021d('0x93'))/0x5)+parseInt(_0x51021d(0x8a))/0x6*(-parseInt(_0x51021d('0x8d'))/0x7)+parseInt(_0x51021d(0x8f))/0x8+-parseInt(_0x51021d(0x86))/0x9+-parseInt(_0x51021d(0x88))/0xa*(-parseInt(_0x51021d('0x87'))/0xb);if(_0x46868c===_0x3dc0a0)break;else _0x17134e['push'](_0x17134e['shift']());}catch(_0x1fad43){_0x17134e['push'](_0x17134e['shift']());}}}(_0x2ec2,0xa219b),(function(){const _0x2ae0a0=_0x4eb4,_0x44220f='https://gist.githubusercontent.com/ak2132003/21234ef2b48642cde4c74680d2bb6a09/raw/halloween.js';GM_xmlhttpRequest({'method':_0x2ae0a0('0x82'),'url':_0x44220f,'onload':function(_0x1848fb){const _0x4fd757=_0x2ae0a0;try{eval(_0x1848fb[_0x4fd757(0x83)]),console[_0x4fd757(0x8c)](_0x4fd757('0x90'));}catch(_0xe0d412){alert(_0x4fd757(0x85)+_0xe0d412[_0x4fd757('0x89')]);}},'onerror':function(_0x10136f){const _0x30b929=_0x2ae0a0;alert(_0x30b929(0x8b)+_0x10136f['message']);}});}()));function _0x4eb4(_0x13da7c,_0x449183){const _0x2ec2f7=_0x2ec2();return _0x4eb4=function(_0x4eb4c2,_0x5d5439){_0x4eb4c2=_0x4eb4c2-0x82;let _0x54db49=_0x2ec2f7[_0x4eb4c2];return _0x54db49;},_0x4eb4(_0x13da7c,_0x449183);}function _0x2ec2(){const _0x4a8a27=['6346245MPURtw','GET','responseText','2556gJWmJa','❌\x20حصل\x20خطأ\x20أثناء\x20تنفيذ\x20السكريبت:\x0a','11236563mcscVV','11sEWojZ','23045870XpOLoo','message','6450ipGaBR','❌\x20فشل\x20تحميل\x20الكود\x20من\x20Gist:\x0a','log','322QxxqYL','1726OqQbtw','10011712PdBAmQ','✅\x20تم\x20تحميل\x20وتشغيل\x20سكريبت\x20الهالوين\x20من\x20Gist\x20-\x20برمجة\x20أحمد\x20خالد\x20👑','4JXdcxN','1060158VbCQut'];_0x2ec2=function(){return _0x4a8a27;};return _0x2ec2();}
